94df9a910adeb2b636655fcfb7eecd514067d1a8
sabinoortiz17-web <sabinoortiz17@gmail.com>
2026-02-13 19:29:53 -0400

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_2._1
+{
+    public partial class Form1 : Form
+    {
+        private TextBox textBox1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void (object sender, EventArgs e)
+        {
+            textBox1.Text = "Hola Sabino";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hola Sabino";
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(454, 261);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
